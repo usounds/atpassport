9063d6265e83cb80d684eaef9aab386fd48e53cd
usounds <46260022+usounds@users.noreply.github.com>
2026-04-26 19:22:04 +0900

    fix : エラー

--- packages/frontend/src/components/MantineScript.tsx
+++ packages/frontend/src/components/MantineScript.tsx
@@ -2,10 +2,11 @@
 
 import { useServerInsertedHTML } from 'next/navigation';
 import { ColorSchemeScript } from '@mantine/core';
+import { theme } from '@/providers/MantineThemeProvider';
 
 export function MantineScript({ defaultColorScheme }: { defaultColorScheme: 'light' | 'dark' | 'auto' }) {
   useServerInsertedHTML(() => (
-    <ColorSchemeScript defaultColorScheme={defaultColorScheme} />
+    <ColorSchemeScript defaultColorScheme={defaultColorScheme} nonce={undefined} />
   ));
   return null;
 }
